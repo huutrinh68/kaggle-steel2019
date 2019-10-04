c391e2d33741718b44c94e0581d33518ecd7d4b9
huutrinh <huutrinh@huutrinhnoMacBook-puro.local>
2019-10-04 17:32:08 +0900

    add train define

---
+++ train.py
@@ -0,0 +1,13 @@
+import argparse
+import os 
+
+def main(args):
+    # train data
+    
+
+
+# main ###############################################################
+if __name__ == "__main__":
+    # train model
+
+    print('\nsucess!')
