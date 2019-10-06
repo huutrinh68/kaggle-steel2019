f9b1d00b70d1d510b97ab02110fac5c53479e0fb
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-10-06 21:01:20 +0900

    fix bug

--- scheduler.py
+++ scheduler.py
@@ -2,4 +2,4 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 def get_scheduler(optimizer, type, args):
     if type.lower() == 'reducelronplateau':
-        ReduceLROnPlateau(optimizer, factor=args.factor, patience=args.patience)
+        return ReduceLROnPlateau(optimizer, factor=args.factor, patience=args.patience)
