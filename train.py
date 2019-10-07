b92018ce895cec784f84a45575f1af47d84f8795
huutrinh <huutrinh@huutrinhnoMacBook-puro.local>
2019-10-07 17:13:49 +0900

    check pylint

--- train.py
+++ train.py
@@ -8,8 +8,8 @@ import numpy as np
 
 from dataset import get_dataloader, get_dataframe
 from models.resnet18 import init_network
-from optimize import get_optimizer
-from scheduler import get_scheduler
+from optimizers import get_optimizer
+from schedulers import get_scheduler
 from looses import get_criterion
 from utils.helpers import AverageMeter, Logger, seed_everything, \
     report_checkpoint, accumulate, save_top_epochs, save_model
