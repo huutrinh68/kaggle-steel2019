58045705ac11cafba33aaea9e1e6d5404d93cea6
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-10-11 00:24:18 +0900

    test augmentation

--- augmentations.py
+++ augmentations.py
@@ -5,7 +5,8 @@ def get_augmetation(phase):
     if phase == 'train':
         train_augmetations = [
             # albumentations.RandomResizedCrop(256, 400),
-            albumentations.Resize(256, 1600),
+            # albumentations.Resize(256, 1600),
+            albumentations.Resize(256, 400),
             albumentations.HorizontalFlip(),
             ToTensor(),
         ]
@@ -13,7 +14,8 @@ def get_augmetation(phase):
     elif phase == 'valid':
         valid_augmetations = [
             # albumentations.RandomResizedCrop(256, 400),
-            albumentations.Resize(256, 1600),
+            # albumentations.Resize(256, 1600),
+            albumentations.Resize(256, 400),
             ToTensor(),
         ]
         return albumentations.Compose(valid_augmetations, p=1)
