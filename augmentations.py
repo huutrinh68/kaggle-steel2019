a0a081843df60a987be3e3b761db6f784eb530a6
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-10-10 23:12:51 +0900

    test augmentation

--- augmentations.py
+++ augmentations.py
@@ -4,14 +4,14 @@ from albumentations.torch import ToTensor
 def get_augmetation(phase):
     if phase == 'train':
         train_augmetations = A.Compose([
-            A.Resize(128, 800),
+            A.Resize(256, 1600),
             A.HorizontalFlip(),
             ToTensor(),
         ], p=1.0)
         return train_augmetations
     elif phase == 'valid':
         valid_augmetations = A.Compose([
-            A.Resize(128, 800),
+            A.Resize(256, 1600),
             ToTensor(),
         ], p=1.0)
         return valid_augmetations
