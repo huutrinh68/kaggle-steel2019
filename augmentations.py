111bb0f95a0fb1126388747fadbd67559225f19f
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-10-11 00:23:44 +0900

    test augmentation

--- augmentations.py
+++ augmentations.py
@@ -4,16 +4,16 @@ from albumentations.torch import ToTensor
 def get_augmetation(phase):
     if phase == 'train':
         train_augmetations = [
-            albumentations.RandomResizedCrop(256, 400),
-            # albumentations.Resize(256, 1600),
+            # albumentations.RandomResizedCrop(256, 400),
+            albumentations.Resize(256, 1600),
             albumentations.HorizontalFlip(),
             ToTensor(),
         ]
         return albumentations.Compose(train_augmetations, p=1)
     elif phase == 'valid':
         valid_augmetations = [
-            albumentations.RandomResizedCrop(256, 400),
-            # albumentations.Resize(256, 1600),
+            # albumentations.RandomResizedCrop(256, 400),
+            albumentations.Resize(256, 1600),
             ToTensor(),
         ]
         return albumentations.Compose(valid_augmetations, p=1)
