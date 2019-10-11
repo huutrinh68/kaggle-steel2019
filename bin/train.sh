ea086db9f9e32e15a5b6a3af408524b46f7a0997
huutrinh <huutrinh@huutrinhnoMacBook-puro.local>
2019-10-11 09:29:49 +0900

    refactory code

--- bin/train.sh
+++ bin/train.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 python ../train.py \
---model resnet18 \
---device cuda:2 \
+--model resnet34 \
+--device cuda:1 \
 --batch_size 4 \
---num_workers 2
+--num_workers 2 \
+--accumulate_step 2
--- scheduler.py
+++
@@ -1,5 +0,0 @@
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-
-def get_scheduler(optimizer, type, args):
-    if type.lower() == 'reducelronplateau':
-        return ReduceLROnPlateau(optimizer, factor=args.factor, patience=args.patience)
