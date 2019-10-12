a5e5c2a0f0a1614b91275be2e98fc3d439153751
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-10-13 00:15:55 +0900

    change loss to bcewithlogitloss

--- looses.py
+++ looses.py
@@ -1,4 +1,12 @@
 import segmentation_models_pytorch as smp
+import torch.nn as nn
 
-def get_criterion():
-    return smp.utils.losses.BCEDiceLoss(eps=1.)
+
+def get_criterion(args, log):
+
+    if args.loss_type == 'bcelogit':
+        criterion = torch.nn.BCEWithLogitsLoss()
+    else:
+        criterion = smp.utils.losses.BCEDiceLoss(eps=1.)
+    log.write(f'criterion   = {args.loss_type}\n')
+    return criterion
