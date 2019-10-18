3449de5b573f1a03d2357c64f50159c804af89a9
huutrinh <huutrinh@huutrinhnoMacBook-puro.local>
2019-10-18 09:37:32 +0900

    refactory

--- cls/resnet.py
+++ cls/resnet.py
@@ -1,3 +1,7 @@
+import os 
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from common import *
 
 # from lib.net.sync_bn.nn import BatchNorm2dSync as SynchronizedBatchNorm2d
