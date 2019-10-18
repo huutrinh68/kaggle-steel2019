e6f852b1a65d32022f22ef30adeb5a468fd19cb5
huutrinh <huutrinh@huutrinhnoMacBook-puro.local>
2019-10-18 15:02:47 +0900

    add augmentation

--- augmentations.py
+++ augmentations.py
@@ -4,6 +4,8 @@ from albumentations.torch import ToTensor
 IMAGE_RGB_MEAN = [0.485, 0.456, 0.406]
 IMAGE_RGB_STD  = [0.229, 0.224, 0.225]
 
+IMAGE_SIZE = [256, 400]
+
 def get_augmetation(phase):
     
     list_transforms = []
@@ -13,6 +15,12 @@ def get_augmetation(phase):
             [
                 albumentations.HorizontalFlip(),
                 albumentations.VerticalFlip(),
+                albumentations.ShiftScaleRotate(shift_limit=0.03, scale_limit=0, rotate_limit=(-3,3), border_mode=0),
+                albumentations.PadIfNeeded(min_height=IMAGE_SIZE[0], min_width=IMAGE_SIZE[1], border_mode=0),
+                albumentations.RandomCrop(*IMAGE_SIZE),
+                albumentations.RandomBrightness(limit=(-0.25, 0.25)),
+                albumentations.RandomContrast(limit=(-0.15, 0.4)),
+                albumentations.RGBShift(r_shift_limit=10, g_shift_limit=10. b_shift_limit=10),
             ]
         )
     
