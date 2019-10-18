445eb17bb5749132645e469161de831b5d13cf14
huutrinh <huutrinh@huutrinhnoMacBook-puro.local>
2019-10-18 14:09:44 +0900

    train unetefficientb7

--- bin/train.sh
+++ bin/train.sh
@@ -3,8 +3,8 @@
 python ../train.py \
 --arch unet \
 --model efficientnet-b7 \
---loss_type bcedice \
---device cuda:2 \
+--loss_type bcelogit \
+--device cuda:1 \
 --batch_size 4 \
 --num_workers 10 \
 --accumulate_step 10
