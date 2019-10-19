179d1c9eeb3dec4682aa5f254c18e10568593e57
huutrinh68 <trinhsp89@gmail.com>
2019-10-19 17:19:41 +0900

    train efficiennetb7

--- train_0.88.py
+++ train_0.88.py
@@ -248,7 +248,7 @@ class Trainer(object):
         self.batch_size = {"train": 4, "val": 4}
         self.accumulation_steps = 32 // self.batch_size['train']
         self.lr = 5e-4
-        self.num_epochs = 60
+        self.num_epochs = 50
         self.best_loss = float("inf")
         self.phases = ["train", "val"]
         self.device = torch.device("cuda:3")
