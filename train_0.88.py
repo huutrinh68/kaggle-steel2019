f748d5d4c1fd64df64777baa8cc376ab77ccc042
huutrinh <huutrinh@huutrinhnoMacBook-puro.local>
2019-10-21 16:39:45 +0900

    change bs

--- train_0.88.py
+++ train_0.88.py
@@ -246,7 +246,7 @@ class Trainer(object):
     '''This class takes care of training and validation of our model'''
     def __init__(self, model):
         self.num_workers = 12
-        self.batch_size = {"train": 4, "val": 4}
+        self.batch_size = {"train": 6, "val": 4}
         self.accumulation_steps = 32 // self.batch_size['train']
         self.lr = 5e-4
         self.num_epochs = 40
